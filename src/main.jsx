ae14e886f5e4ce6e66511ee0974ca400c81e408a
MiguelAngel <miguelalvarezfranco36@gmail.com>
2025-02-22 09:08:26 -0500

    tercer commit se Realizando paso a paso la Aplicacion

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { PrimeraApp } from './PrimeraApp.JSX';
+
+
+
 //import { HolaMundo }  from './HolaMundo';
 
 
+import './styles.css';
 
 
 
@@ -11,7 +14,7 @@ import { PrimeraApp } from './PrimeraApp.JSX';
 
 ReactDOM.createRoot( document.getElementById('root')).render(
     <React.StrictMode>
-        <PrimeraApp />
+        <PrimeraApp title />
     </React.StrictMode>
 );
 
